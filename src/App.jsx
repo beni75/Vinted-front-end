8aadb10fcce5eb28cbee641c049dde7fd413c5cc
Your Name <you@example.com>
2023-08-30 06:15:39 +0200

    Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Payment from "./pages/Payment";
 //import des composents
 import Header from "./components/Header";
 import Loader from "./components/Loader";
+import CheckoutForm from "./components/checkoutForm";
 
 function App() {
   const [userToken, setUserToken] = useState(Cookies.get("userToken") || null);
